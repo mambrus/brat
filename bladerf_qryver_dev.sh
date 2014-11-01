2fc53ab53a39c2baaa759f1ab0ed7dc351912338
Michael Ambrus <ambrmi09@gmail.com>
2014-11-01 14:58:42 +0100

    Device probing and format-parsing corrected

--- bladerf_qryver_dev.sh
+++ bladerf_qryver_dev.sh
@@ -7,10 +7,21 @@ INCLUDE funcs
 
 TYPE=$1
 
-if [ "X${1}" == "Xfpga" ] || [ "X${1}" == "Xfirmware" ]; then
+DEV_SERIAL=$(bladeRF-cli -p 2>/dev/null | grep Serial | awk '{print $2}')
+DEV_ATTACHED=$(bladeRF-cli -p 2>&1 | grep "No device")
+
+
+if [ "X${DEV_ATTACHED}" != "X" ] || [ "X${DEV_SERIAL}" == "X" ]; then
+	echo "Device error: ${DEV_ATTACHED} ${DEV_SERIAL}" 1>&2
+	exit 1
+fi
+
+if [ "X${1}" == "Xfpga" ]; then
+	dev_versionsof $TYPE | head -n1 | awk '{print $1}'
+elif [ "X${1}" == "Xfirmware" ]; then
 	dev_versionsof $TYPE | head -n1 | awk '{print $2}'
 else
-	echo "Error: $(basename $0) expects one argument [firmware|fpga]" 1>&2
+	echo "Error: $(basename $0) expects one argument [\"firmware\"|\"fpga\"]" 1>&2
 	exit 1
 fi
 
